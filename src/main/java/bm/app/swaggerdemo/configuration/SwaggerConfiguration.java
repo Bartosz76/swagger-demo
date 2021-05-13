bcd8669bbcddb558125c5aea7c03b1412a4c043d
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-05-13 15:59:00 +0200

    Added initial Swagger customization for the API.

--- src/main/java/bm/app/swaggerdemo/configuration/SwaggerConfiguration.java
+++ src/main/java/bm/app/swaggerdemo/configuration/SwaggerConfiguration.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -16,7 +19,20 @@ public class SwaggerConfiguration {
                 .select()
                 .paths(PathSelectors.ant("/api/*"))
                 .apis(RequestHandlerSelectors.basePackage("bm.app.swaggerdemo"))
-                .build();
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Pokemon API",
+                "Sample API for Swagger learning",
+                "1.0",
+                "Free to use",
+                new springfox.documentation.service.Contact("Bartosz Mierzwinski", "https://github.com/Bartosz76", "mierzwinski.b@gmail.com"),
+                "API License",
+                "https://github.com/Bartosz76",
+                Collections.emptyList());
     }
 
 }
